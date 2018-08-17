04cc3acf4dd3f86dd6b482874e98f0a848e8fef5
MiYanni <miyanni@microsoft.com>
2018-08-17 14:18:13 -0700

    Moved all Stack modules into StackAdmin folder.

---
+++ Compute/Commands.Compute.Test/ScenarioTests/DscExtensionTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Xunit;
+
+namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
+{
+    public class DscExtensionTests
+    {
+    }
+}
