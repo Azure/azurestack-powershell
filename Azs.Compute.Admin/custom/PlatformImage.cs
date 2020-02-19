36d2a68499cb1dd9dc15d9656bf092c4ada9f1d0
Hannah Murphy <hamurphy@microsoft.com>
2020-02-19 11:33:53 -0800

    adding headers to cs files

--- Azs.Compute.Admin/custom/PlatformImage.cs
+++ Azs.Compute.Admin/custom/PlatformImage.cs
@@ -1,3 +1,8 @@
+//---------------------------------------------------------------------------------------------
+/// Copyright (c) Microsoft Corporation. All rights reserved.
+/// Licensed under the MIT License. See License.txt in the project root for license information.
+//---------------------------------------------------------------------------------------------
+
 namespace Microsoft.Azure.PowerShell.Cmdlets.ComputeAdmin.Models.Api20151201Preview
 {
     using System;
