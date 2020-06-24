a360b4809886b6ad33908f38b561f2408101f9b4
Vishnu Priya Ananthu Sundaram <viananth@microsoft.com>
2020-06-24 15:25:07 -0700

    Move default parameter loading to loadEnvJson.ps1

--- src/Azs.KeyVault.Admin/test/Get-AzsKeyvaultQuota.Tests.ps1
+++ src/Azs.KeyVault.Admin/test/Get-AzsKeyvaultQuota.Tests.ps1
@@ -1,21 +1,5 @@
 # Load Default parameter values
-
-$envFile = 'env.json'
-if ($TestMode -eq 'live') {
-    $envFile = 'localEnv.json'
-}
-
-if (Test-Path -Path (Join-Path $PSScriptRoot $envFile)) {
-    $envFilePath = Join-Path $PSScriptRoot $envFile
-} else {
-    $envFilePath = Join-Path $PSScriptRoot '..\$envFile'
-}
-$env = @{}
-if (Test-Path -Path $envFilePath) {
-    $env = Get-Content (Join-Path $PSScriptRoot $envFile) | ConvertFrom-Json
-    $PSDefaultParameterValues=@{"*:SubscriptionId"=$env.SubscriptionId; "*:Tenant"=$env.Tenant}
-    $PSDefaultParameterValues.Add("*:Location", $env.Location)
-}
+. (Join-Path $PSScriptRoot 'loadEnvJson.ps1')
 
 $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzsKeyvaultQuota.Recording.json'
 $currentPath = $PSScriptRoot
