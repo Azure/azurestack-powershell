d16c1561210b75895a92f0844bc4a60b6ad2f9d8
larisaborodina <larisab@microsoft.com>
2021-08-04 18:35:19 -0700

    Add pester tests for all the cmdlets.

---
+++ src/Azs.ContainerRegistry.Admin/test/Read-AzsContainerRegistry.Tests.ps1
@@ -0,0 +1,47 @@
+$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+if (-Not (Test-Path -Path $loadEnvPath)) {
+    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+}
+. ($loadEnvPath)
+$TestRecordingFile = Join-Path $PSScriptRoot 'Read-AzsContainerRegistry.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
+Describe 'Read-AzsContainerRegistry' {
+    BeforeEach {
+
+        function ValidateRegistry {
+            param(
+                [Parameter(Mandatory = $true)]
+                $Registry
+            )
+            $registry				| Should Not Be $null
+            $registry.CreationDate	| Should Not Be $null
+            $registry.Id	        | Should Not Be $null
+            $registry.Location	    | Should Not Be $null
+            $registry.Name	        | Should Not Be $null
+            $registry.PropertiesName| Should Not Be $null
+            $registry.RegistryId	| Should Not Be $null
+            $registry.RegistrySizeInByte | Should Not Be $null
+            $registry.ResourceGroup	| Should Not Be $null
+            $registry.SubscriptionId| Should Not Be $null
+            $registry.Type	        | Should Not Be $null
+        }
+    }
+
+    AfterEach {
+        $global:Client = $null
+    }
+
+    It "TestGetContainerRegistryRegistry" -Skip:$('TestReadContainerRegistry' -in $global:SkippedTests) {
+        $global:TestName = 'TestReadContainerRegistry'
+
+        $result = Read-AzsContainerRegistry  
+        $result  | Should Not Be $null
+        ValidateRegistry -Registry $result
+    }
+}
