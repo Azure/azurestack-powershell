751b12955234d2e29349b8b27ccad00e7e7b747f
Bhuvaneswari Santharam <bhsantha@microsoft.com>
2021-08-10 17:55:03 -0700

    Adding AKS admin pester test

---
+++ src/Azs.ContainerService.Admin/test/Get-AzsContainerService.Tests.ps1
@@ -0,0 +1,38 @@
+$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+if (-Not (Test-Path -Path $loadEnvPath)) {
+    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+}
+. ($loadEnvPath)
+$TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzsContainerService.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
+Describe 'Get-AzsContainerService' {
+    BeforeEach {
+        function ValidateContainerService {
+            param(
+                [Parameter(Mandatory = $true)]
+                $ManagedClusters
+            )
+            $managedClusters						| Should Not Be $null
+            $capacity.orchestratorVersion	        | Should Not Be $null
+            $capacity.provisioningState         	| Should Not Be $null
+        }
+    }
+
+    AfterEach {
+        $global:Client = $null
+    }
+
+    It "TestGetContainerService" -Skip:$('TestGetContainerService' -in $global:SkippedTests) {
+        $global:TestName = 'TestGetContainerService'
+
+        $result = Get-AzsContainerService  
+        $result  | Should Not Be $null
+        ValidateContainerService -ManagedClusters $result
+    }
+}
