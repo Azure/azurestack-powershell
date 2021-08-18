ae038e2b3671ecc74deb7cbd2a5d995e7a2fca9b
unknown <Administrator@n22r1007-DVM.n22r1007.masd.stbtest.microsoft.com>
2021-08-18 21:15:50 +0000

    Adding tests for 'Features'

---
+++ src/Azs.Compute.Admin/test/Disable-AzsComputeUserSubscriptionFeature.Tests.ps1
@@ -0,0 +1,26 @@
+$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+if (-Not (Test-Path -Path $loadEnvPath)) {
+    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+}
+. ($loadEnvPath)
+$TestRecordingFile = Join-Path $PSScriptRoot 'Disable-AzsComputeUserSubscriptionFeature.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
+Describe 'Disable-AzsComputeUserSubscriptionFeature' {
+    It 'TestDisableComputeUserSubscriptionFeature' -Skip:$('TestDisableComputeUserSubscriptionFeature' -in $global:SkippedTests) {
+        $global:TestName = 'TestDisableComputeUserSubscriptionFeature'
+
+        $tenantSubscriptionId = [guid]::NewGuid().ToString()
+        $feature = Get-AzsComputeFeature -Name Microsoft.Compute.EmergencyVMAccess -Location $env.Location
+        $originalLastSubscriptionId = $feature.EnabledTenantSubscriptionId[-1]
+        Enable-AzsComputeUserSubscriptionFeature -TenantSubscriptionId $tenantSubscriptionId -FeatureName Microsoft.Compute.EmergencyVMAccess -Location $env.Location -SubscriptionId $env.SubscriptionId
+        Disable-AzsComputeUserSubscriptionFeature -TenantSubscriptionId $tenantSubscriptionId -FeatureName Microsoft.Compute.EmergencyVMAccess -Location $env.Location -SubscriptionId $env.SubscriptionId
+        $feature = Get-AzsComputeFeature -Name Microsoft.Compute.EmergencyVMAccess -Location $env.Location -SubscriptionId $env.SubscriptionId
+        $feature.EnabledTenantSubscriptionId[-1] | Should Be $originalLastSubscriptionId
+    }
+}
---
+++ src/Azs.Compute.Admin/test/Enable-AzsComputeUserSubscriptionFeature.Tests.ps1
@@ -0,0 +1,25 @@
+$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+if (-Not (Test-Path -Path $loadEnvPath)) {
+    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+}
+. ($loadEnvPath)
+$TestRecordingFile = Join-Path $PSScriptRoot 'Enable-AzsComputeUserSubscriptionFeature.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
+Describe 'Enable-AzsComputeUserSubscriptionFeature' {
+    It 'TestEnableComputeUserSubscriptionFeature' -Skip:$('TestEnableComputeUserSubscriptionFeature' -in $global:SkippedTests) {
+        $global:TestName = 'TestEnableComputeUserSubscriptionFeature'
+
+        $tenantSubscriptionId = [guid]::NewGuid().ToString()
+        $feature = Get-AzsComputeFeature -Name Microsoft.Compute.EmergencyVMAccess -Location $env.Location
+        $feature.EnabledTenantSubscriptionId[-1] | Should Not Be $tenantSubscriptionId
+        Enable-AzsComputeUserSubscriptionFeature  -TenantSubscriptionId $tenantSubscriptionId -FeatureName Microsoft.Compute.EmergencyVMAccess -Location $env.Location -SubscriptionId $env.SubscriptionId
+        $feature = Get-AzsComputeFeature -Name Microsoft.Compute.EmergencyVMAccess -Location $env.Location -SubscriptionId $env.SubscriptionId
+        $feature.EnabledTenantSubscriptionId[-1] | Should Be $tenantSubscriptionId
+    }
+}
